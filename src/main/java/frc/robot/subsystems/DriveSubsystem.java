308a4aad7f780c6dacd53151c47a3e5927853efb
OddDuck <alexh@ieee.org>
2024-01-13 12:08:14 -0600

    vision

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -79,20 +79,20 @@ public class DriveSubsystem extends SubsystemBase {
     SmartDashboard.putData(m_field);
 
     // Configure the AutoBuilder last
-    AutoBuilder.configureHolonomic(
-      this::getPose, // Robot pose supplier
-      this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
-      this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-      this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-      new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-          new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
-          new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
-          4.5, // Max module speed, in m/s
-          0.4, // Drive base radius in meters. Distance from robot center to furthest module.
-          new ReplanningConfig() // Default path replanning config. See the API for the options here
-      ),
-      this // Reference to this subsystem to set requirements
-    );
+    // AutoBuilder.configureHolonomic(
+    //   this::getPose, // Robot pose supplier
+    //   this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
+    //   this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+    //   this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+    //   new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+    //       new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+    //       new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+    //       4.5, // Max module speed, in m/s
+    //       0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+    //       new ReplanningConfig() // Default path replanning config. See the API for the options here
+    //   ),
+    //   this // Reference to this subsystem to set requirements
+    // );
     // Reference: https://www.chiefdelphi.com/t/has-anyone-gotten-pathplanner-integrated-with-the-maxswerve-template/443646
   }
 
