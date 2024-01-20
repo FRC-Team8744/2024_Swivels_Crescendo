fb79a73bf1c3f3cb4fe6f1f9d486a09971179f81
OddDuck <alexh@ieee.org>
2024-01-20 13:33:00 -0600

    coolfunnystuff

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -5,13 +5,36 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LimelightHelpers;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Vision extends SubsystemBase {
-  /** Creates a new Vision. */
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry ty = table.getEntry("ty");
+  NetworkTableEntry ta = table.getEntry("ta");
+  NetworkTableEntry tid = table.getEntry("tid");
+
+
   public Vision() {}
 
   @Override
   public void periodic() {
+    double tax = LimelightHelpers.getTX("");
+
+    double x = tx.getDouble(0.0);
+double y = ty.getDouble(0.0);
+double area = ta.getDouble(0.0);
+double AprilNumber = tid.getDouble(0.0);
+
+//post to smart dashboard periodically
+SmartDashboard.putNumber("LimelightX", x);
+SmartDashboard.putNumber("LimelightY", y);
+SmartDashboard.putNumber("LimelightArea", area);
+SmartDashboard.putNumber("tid", AprilNumber);
     // This method will be called once per scheduler run
   }
 }
